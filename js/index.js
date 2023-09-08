611651d3db0d8f57c097ddc7bc1e2d9831eb80da
Vignesh227 <96369223+Vignesh227@users.noreply.github.com>
2023-09-08 14:41:41 +0530

    Made the page responsive

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 var typed = new Typed(".autotyped", {
 
-    strings: ['Arthur Morgan.', 'a Web Developer.', 'a Data Analyst.'],
+    strings: ['Vignesh Pandian.', 'a Web Developer.', 'a Data Analyst.'],
     typeSpeed: 100,
     backSpeed: 50,
     loop:true
