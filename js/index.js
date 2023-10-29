c3d9750bb1b5720a7bc366cd48d71ac4dca2a389
Vignesh Pandian <tvigneshpandian27@gmail.com>
2023-10-29 14:48:59 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 var typed = new Typed(".autotyped", {
 
-    strings: ['Vignesh Pandian.', 'a Web Developer.', 'a Data Analyst.'],
+    strings: ['Vignesh Pandian.', 'a Web Developer.', 'a Data Science Student.'],
     typeSpeed: 100,
     backSpeed: 50,
     loop:true
